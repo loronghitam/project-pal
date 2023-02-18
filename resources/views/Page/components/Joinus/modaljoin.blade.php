3b803c3603361c5d8e53253e27e609b6482691fc
loronghitam <sutio.mudianro@gmail.com>
2023-02-18 13:35:52 +0700

    fix(style): memperbaiki modal bergabung
    [skip ci]

--- resources/views/Page/components/Joinus/modaljoin.blade.php
+++ resources/views/Page/components/Joinus/modaljoin.blade.php
@@ -38,7 +38,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="createSubmit">Donate</button>
+                    <button type="button" class="btn btn-primary" id="createSubmit">Konfirmasi</button>
                 </div>
             </div>
         </div>
--- resources/views/Page/page/blogdetail.blade.php
+++ resources/views/Page/page/blogdetail.blade.php
@@ -11,11 +11,7 @@
         <div class="container">
             <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
                 <div class="col-first">
-                    <h1>Blog Page</h1>
-                    <nav class="d-flex align-items-center">
-                        <a href="{{ url("/") }}">Home<span class="lnr lnr-arrow-right"></span></a>
-                        <a href="{{ url("/berita") }}">Blog</a>
-                    </nav>
+                    <h1>Berita</h1>
                 </div>
             </div>
         </div>
